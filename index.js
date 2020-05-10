c04760daa1e41d3159607cbf6f55f3092a7eeba3
Babak Gholamzadeh <babak.gholamzadeh92@gmail.com>
2020-05-11 00:47:33 +0430

    overwrite the log method on console object to be able to process StyledString objects

--- index.js
+++ index.js
@@ -4,3 +4,17 @@ String.prototype.styles = function(stylesObject) {
   return new StyledString(this, stylesObject);
 }
 
+const log = console.log;
+console.log = function(...args) {
+  const newArgs = [];
+  for(let i = 0; i < args.length; i++) {
+    if(args[i] instanceof StyledString) {
+      let string = `%c${args[i].string}`;
+      let styles = args[i].styles;
+      newArgs.push(string, styles);
+    } else {
+      newArgs.push(args[i]);
+    }
+  }
+  log(...newArgs);
+}
